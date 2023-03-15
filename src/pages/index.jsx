0319aeec0c7e0aaa23897fba2781663713342fe8
Kevin3874 <kzhang77@jh.edu>
2023-03-14 22:17:14 -0400

    Updated title and icon, updated image path

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,10 +14,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>TaxPal - Accounting made simple for small businesses</title>
+        <title>Quest2Learn - Augmented learning for modern classrooms</title>
         <meta
           name="description"
-          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+          content="Quest2Learn is a revolutionary augmented reality platform that allows teachers to transform any environment into a laboratory for learning science.."
         />
       </Head>
       <Header />
